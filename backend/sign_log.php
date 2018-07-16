db152a26639adc8a86dcaa8e9d639df47252bbd4
Daniell Levy <daniell.levy@hotmail.com>
2018-07-16 18:01:49 +0900

    added checks for login

--- backend/sign_log.php
+++ backend/sign_log.php
@@ -5,19 +5,21 @@
     $servername = 'localhost';
     $username = 'root';
     $password = '';
-    $dbname = 'weventory';
+    $dbname = 'weventory'; 
 
     if (isset($receive->user_bday)){
         try{
             $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             $stmt = $connection->prepare("
-                INSERT INTO `users`
-                    (`username`, `password`, `firstname`, `lastname`, `birthday`, `email`, `bio`, `pic`,`interests`)
+                INSERT INTO `account`
+                    (`user_login`, `password`, `f_name`, `l_name`, `b_day`, `email`, `bio`, `pic`,`interests`)
                 VALUES
                     (:un, :pw, :firn, :famn, :birthday, :email, :bio, :pic, :interests);");
-            $lowerCase = strtolower($receive->user_bday);
+            $lowerCase = strtolower($receive->user_login);
+            $salt = "ThisIsRealSaltyIlya";
+            $hashedPswrd = hash('sha256', $salt.$receive->user_pswrd);
             $stmt->bindParam(':un', $lowerCase);
-            $stmt->bindParam(':pw', $receive->user_pswrd);
+            $stmt->bindParam(':pw', $hashedPswrd);
             $stmt->bindParam(':firn', $receive->user_firstName);
             $stmt->bindParam(':famn', $receive->user_lastName);
             $stmt->bindParam(':birthday', $receive->user_bday);
@@ -45,14 +47,56 @@
         catch(PDOException $e) {
         }
     }
-    // else{
-    //     try{
-    //         session_start()
+    else if(isset($receive->loginCheck)){
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $stmt = $connection->prepare("
+                SELECT `user_login` 
+                FROM `account`
+                WHERE `user_login` = :uc;");
+        $lowerCase = strtolower($receive->loginCheck);
+        $stmt->bindParam(':uc', $lowerCase);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if(isset($result['user_login'])){
+            echo 'exists';
+        }
 
-    //     }
-    //     catch(PDOException $e) {
-    //     }
-    // }
+        $connection = null;
+        $stmt = null;
+
+    }
+    else if($receive->user_login){
+        try{
+            $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $stmt = $connection->prepare("
+                    SELECT `user_login`, `password` 
+                    FROM `account`
+                    WHERE `user_login` = :uc AND `password` = :pw;");
+            $lowerCase = strtolower($receive->user_login);
+            $salt = "ThisIsRealSaltyIlya";
+            $hashedForCheck = hash('sha256', $salt.$receive->user_pswrd);
+            $stmt->bindParam(':uc', $lowerCase);
+            $stmt->bindParam(':pw', $hashedForCheck);
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            if (isset($result['user_login'])){
+                session_start();
+                $_SESSION['user_login'] = $result['user_login'];
+                echo 'YAY';
+            }
+            else{
+                echo 'OI';
+            }
+            
+            
+        }
+        catch(PDOException $e) {
+        }
+    }
+    else
+        echo("Wrong request")
+    
 
 
 ?>
